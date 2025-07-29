8994726bcb53d41652bb094a211a9f7129c736a7
ckstart <493261380@qq.com>
2025-07-29 23:11:48 +0800

    '开始开发ap'

---
+++ components/funs-one/funs-one.vue
@@ -0,0 +1,44 @@
+<template>
+		<view class="funs">
+			<navigator class="onefun">
+				<image class="pic" src="/static/userFuns/meet.png" mode="aspectFill"></image>
+				<view class="txt">会议</view>
+			</navigator>
+		</view>
+	
+</template>
+
+<script setup>
+ 
+</script>
+
+<style lang="scss" scoped>
+.funs{
+	 
+	.onefun{
+		height: 130rpx;
+		width: 100rpx;
+		overflow: hidden;
+		// border-radius: 10rpx;
+		display: flex;
+		flex-direction: column;
+		 align-items: center;
+		background-color: cornflowerblue;
+		.pic{
+			margin-top: 10rpx;
+			width: 70rpx;
+			height: 70rpx;
+			background-color: yellow;
+		}
+		.txt{
+			height: 60rpx;
+			background-color: green;
+			display: flex;
+			 justify-content: center;
+			
+			font-size: 22rpx;
+			
+		}
+	}
+}
+</style>
---
+++ pages/user/user.vue
@@ -0,0 +1,73 @@
+<template>
+	<view class="userLayout">
+		<view class="userInfo">
+			<view class="avator">
+				<image src="/static/logo.png" mode="aspectFill"></image>
+			</view>
+			<view class="info">
+				<view class="name">
+					张三
+				</view>
+				<view class="level">
+					一级警员
+				</view>
+			</view>
+		</view>
+		<view class="userfuns">
+		 <funs-one v-for="item in 7"></funs-one>
+		</view>
+	</view>
+</template>
+
+<script setup>
+	 
+</script>
+
+<style lang="scss">
+.userLayout{
+	
+	.userInfo{
+		display: flex;
+		padding: 50rpx 0;
+		.avator{
+			width: 160rpx;
+			height: 160rpx;
+			overflow: hidden;
+			border-radius: 10%;
+			margin-left: 30rpx;
+			image{
+				width: 100%;
+				height: 100%;
+			}
+			
+		}
+		.info{
+			margin-left: 50rpx;
+			.name{
+				font-size: 40rpx;
+				font-weight: 550;
+				
+			}
+			.level{
+				//font-weight: 600;
+				margin-top: 15rpx;
+				font-size: 30rpx;
+				color: #aaa;
+			}
+		}
+	}
+	.userfuns{
+	 
+		width: 700rpx;
+		margin: 10rpx auto;
+		padding-left: 10rpx;
+		border: 1px solid #eee;
+		border-radius: 10rpx;
+		box-shadow: 0 0 30rpx rgba(0, 0, 0, 0.15);
+		background-color: white;
+		display: grid;
+		gap: 5rpx 10rpx;
+		grid-template-columns: repeat(4,1fr);
+	}
+}
+</style>
