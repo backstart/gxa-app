4677e75c344963f71e6891cd894a11d140360a3d
ckstart <493261380@qq.com>
2025-08-20 20:45:35 +0800

    增加工作圈

--- pages/work/work.vue
+++ pages/work/work.vue
@@ -1,10 +1,16 @@
 <template>
 	<view>
-		
+		<!-- leftTitle:标题  icon：左边图标 @click：点击事件  -->
+		<cc-wxMenuBtn leftTitle="工作圈" icon="/static/work/朋友圈.png" @click="goFunctionClick(0)"></cc-wxMenuBtn>
+		 
 	</view>
 </template>
 
 <script setup>
+ 
+ function goFunctionClick(e){
+	 console.log(e);
+ }
  
 </script>
 
