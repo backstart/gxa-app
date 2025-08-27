6ac189a9d1da020dc5e3f93a189372e3bdf47f9d
ckstart <493261380@qq.com>
2025-08-28 00:46:33 +0800

    场所细分

--- pages/funs/funs.vue
+++ pages/funs/funs.vue
@@ -1,13 +1,140 @@
- <template>
-	<view>
-		
+
+<template>
+	<view class="page">
+		<view class="statuBar" :style="{height: barheight+'px'}"></view>
+		<view class="head">
+			<view class="title">
+				<view class="txt1">
+					龙井坊KTV
+				</view>
+			
+			</view>
+			
+			<view class="pepoleinfo">
+				<view class="flexrow">
+					<view class="txt_gray">
+						负责人：
+					</view>
+					<view class="txtclick">
+						张三
+					</view>
+				</view>
+				 <view  class="flexrow">
+				 	<view class="txt_gray">
+				 		电话：
+				 	</view>
+				 	<view class="txtclick">
+				 		123456789
+				 	</view>
+				 </view>
+			</view>
+			
+			<view class="pepoleinfo">
+				<view class="flexrow">
+					<view class="txt_gray">
+						管理员：
+					</view>
+					<view class="txtclick">
+						李四
+					</view>
+				</view>
+				 <view  class="flexrow">
+				 	<view class="txt_gray">
+				 		电话：
+				 	</view>
+				 	<view class="txtclick">
+				 		1855522365
+				 	</view>
+				 </view>
+			</view>
+			
+			<view class="address flexrow">
+				<view class="txt_gray">
+					地址：
+				</view>
+				<view class="txtclick">
+					XX市XX区XX街道XX小区3栋2单元
+				</view>
+			</view>
+			<view class="time flexrow">
+				<view class="txt_gray">
+					最近检查时间：
+				</view>
+				<view class="txt_gray_after">
+					2025年8月21日20时36分
+				</view>
+				
+			</view>
+			<com-tag></com-tag>
+			
+			
+			
+			
+		</view>
+		<view class="body">
+			<l-grid>
+			    <l-grid-item text="从业人员" :dot="true" image="/static/userFuns/meet.png" />
+			    <l-grid-item text="档案" badge="5" image="/static/userFuns/oa.png" />
+			    <l-grid-item text="检查记录" badge="15" image="/static/userFuns/leave.png" />
+			    <l-grid-item text="关联警情" badge="New" image="/static/userFuns/duty.png" />
+			</l-grid>
+		</view>
 	</view>
 </template>
 
 <script setup>
- 
+ import {
+ 	ref
+ } from 'vue';
+ import {
+ 	getStatusBarHeight
+ } from "@/utils/system.js";
+ const barheight = ref(getStatusBarHeight());
 </script>
 
 <style lang="scss" scoped>
+	.page {
+		height: 100%;
+		width: 100%;
+	}
 
+.head{
+	border: 1px solid #eee;
+	border-radius: 10rpx;
+	box-shadow: 0 0 30rpx rgba(0, 0, 0, 0.15);
+	background-color: white;
+	margin: 10rpx 20rpx;
+	padding: 10rpx;
+}
+	.title {
+		margin: 10rpx 10rpx;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+
+	.innum {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
+	.pepoleinfo{
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		margin-bottom: 8px;
+	}
+	.flexrow{
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+	.introduction{
+		margin-bottom: 10px;
+	}
+	.address{
+		margin-bottom: 8px;
+	}
 </style>
