a8608146df74d7e338f915893b0bd0a76da5739b
ckstart <493261380@qq.com>
2025-08-30 16:44:38 +0800

    增加娱乐场所的检查记录浏览列表，把列表抽离成组件的模式

---
+++ components/browselist/browselist.vue
@@ -0,0 +1,96 @@
+<template>
+	<view class="venuelist flexrow" v-for="item in list" :key="item.id" @click="clickitem(item)">
+		<image class="titleimg" :src="item.img" mode=" aspectFill"
+			:style="{height: '200rpx',width:'200rpx'}"></image>
+		<view class="txtlist ">
+			<view class="txttile txt1">
+				{{item.title}}
+			</view>
+			<view class="txtmain txt_gray_after">
+				{{item.maintxt}}
+			</view>
+			<view class="txtlistbot flexrow">
+				<view class="txtlistbottime txt_gray_small">
+					检查时间：{{item.time}}
+				</view>
+				<view class="txtlistbotstaff txt_gray_small">
+					人员：{{item.inspector}}
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script setup>
+ defineProps({
+ 	 list:{
+ 		 type:[{}],
+ 		 default(){
+ 		 	return [{title:"安全检查",
+			maintxt:"抽查了三间包厢，暂未发现问题",
+			img:"/static/demofile/包房.jpg",
+			time:"2025-9-17",
+			inspector:"张三、李四",
+			id:'1'
+			}]
+ 		 }
+ 	 }
+ })
+ const emit=defineEmits(["clickitem"]);
+ 
+ function clickitem(e){
+ 	 console.log(e);
+ 	 emit("clickitem",e);
+ }
+ 
+</script>
+
+<style lang="scss" scoped>
+@import url("../../common/style/common-style.scss");
+	.bottomlist {
+		width: 100%;
+	}
+
+	.titleimg {
+		border-radius: 10rpx;
+	}
+
+	.venuelist {
+		margin: 15rpx 10rpx;
+		width: 730rpx;
+		height: 200rpx;
+	}
+
+	.txtlist {
+		margin-left: 10rpx;
+		
+		height: 200rpx;
+		width: 530rpx;
+		padding-left: 5rpx;
+		padding-right: 10rpx;
+
+
+	}
+
+	.txtlistbot {
+		width: 100%;
+		justify-content: space-between;
+		
+		height: 45rpx;
+
+	}
+
+	.txttile {
+		margin-top: 5rpx;
+		height: 50rpx;
+		
+	}
+
+	.txtmain {
+		height: 100rpx;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+
+	}
+</style>
--- pages/funs/funs.vue
+++ pages/funs/funs.vue
@@ -1,4 +1,3 @@
-
 <template>
 	<view class="page">
 		<view class="statuBar" :style="{height: barheight+'px'}"></view>
@@ -7,9 +6,9 @@
 				<view class="txt1">
 					龙井坊KTV
 				</view>
-			
+
 			</view>
-			
+
 			<view class="pepoleinfo">
 				<view class="flexrow">
 					<view class="txt_gray">
@@ -19,16 +18,16 @@
 						张三
 					</view>
 				</view>
-				 <view  class="flexrow">
-				 	<view class="txt_gray">
-				 		电话：
-				 	</view>
-				 	<view class="txtclick">
-				 		123456789
-				 	</view>
-				 </view>
+				<view class="flexrow">
+					<view class="txt_gray">
+						电话：
+					</view>
+					<view class="txtclick">
+						123456789
+					</view>
+				</view>
 			</view>
-			
+
 			<view class="pepoleinfo">
 				<view class="flexrow">
 					<view class="txt_gray">
@@ -38,16 +37,16 @@
 						李四
 					</view>
 				</view>
-				 <view  class="flexrow">
-				 	<view class="txt_gray">
-				 		电话：
-				 	</view>
-				 	<view class="txtclick">
-				 		1855522365
-				 	</view>
-				 </view>
+				<view class="flexrow">
+					<view class="txt_gray">
+						电话：
+					</view>
+					<view class="txtclick">
+						1855522365
+					</view>
+				</view>
 			</view>
-			
+
 			<view class="address flexrow">
 				<view class="txt_gray">
 					地址：
@@ -63,59 +62,82 @@
 				<view class="txt_gray_after">
 					2025年8月21日20时36分
 				</view>
-				
+
 			</view>
 			<com-tag></com-tag>
-			
-			
-			
-			
+
+
+
+
 		</view>
 		<view class="body">
-			<l-grid >
-				 <l-grid-item @click="lgClick('1')" text="检查记录" badge="15" image="/static/venue/检查记录 .png" />
-			    <l-grid-item @click="lgClick('1')" text="从业人员"  image="/static/venue/人员信息.png" />
-			    <l-grid-item @click="lgClick('1')" text="档案" badge="5" image="/static/venue/档案.png" />
-			    <l-grid-item @click="lgClick('1')" text="关联警情" badge="New" image="/static/venue/关联警情.png" />
+			<l-grid :inset="true">
+				<l-grid-item @click="lgClick('1')" text="检查记录" badge="15" image="/static/venue/检查记录 .png" />
+				<l-grid-item @click="lgClick('1')" text="从业人员" image="/static/venue/人员信息.png" />
+				<l-grid-item @click="lgClick('1')" text="档案" badge="5" image="/static/venue/档案.png" />
+				<l-grid-item @click="lgClick('1')" text="关联警情" badge="New" image="/static/venue/关联警情.png" />
 			</l-grid>
 		</view>
-		
+		<view class="bottomlist">
+			<browselist :list="list" @clickitem="clickitem"></browselist>
+		</view>
 	</view>
 </template>
 
 <script setup>
- import {
- 	ref
- } from 'vue';
- import {
- 	getStatusBarHeight
- } from "@/utils/system.js";
- const barheight = ref(getStatusBarHeight());
- 
- function lgClick(e){
-	 console.log(e);
- }
- 
+	import {
+		ref
+	} from 'vue';
+	import {
+		getStatusBarHeight
+	} from "@/utils/system.js";
+	const barheight = ref(getStatusBarHeight());
 
- 
+	// 点击图标
+	function lgClick(e) {
+		console.log(e);
+	}
+	// 点击检查记录列表
+	function clickitem(e) {
+
+	}
+
+	const list = ref([{
+		title: "安全检查",
+		maintxt: "抽查了三间包厢，暂未发现问题",
+		img: "/static/demofile/包房.jpg",
+		time: "2025-9-17",
+		inspector: "张三、李四",
+		id: '1'
+	}, {
+		title: "消防检查",
+		maintxt: "干粉灭火器3台，均未过期",
+		img: "/static/demofile/ktv灭火器.jpg",
+		time: "2025-9-17",
+		inspector: "李四",
+		id: '2'
+	}])
 </script>
 
 
 
 <style lang="scss" scoped>
+	@import url("../../common/style/common-style.scss");
+
 	.page {
 		height: 100%;
-		width: 100%;
+		width: 750rpx;
 	}
 
-.head{
-	border: 1px solid #eee;
-	border-radius: 10rpx;
-	box-shadow: 0 0 30rpx rgba(0, 0, 0, 0.15);
-	background-color: white;
-	margin: 10rpx 20rpx;
-	padding: 10rpx;
-}
+	.head {
+		border: 1px solid #eee;
+		border-radius: 10rpx;
+		box-shadow: 0 0 30rpx rgba(0, 0, 0, 0.15);
+		background-color: white;
+		margin: 10rpx 20rpx;
+		padding: 10rpx;
+	}
+
 	.title {
 		margin: 10rpx 10rpx;
 		display: flex;
@@ -129,22 +151,12 @@
 		justify-content: center;
 		align-items: center;
 	}
-	.pepoleinfo{
+
+	.pepoleinfo {
 		display: flex;
 		flex-direction: row;
 		align-items: center;
 		justify-content: space-between;
-		margin-bottom: 8px;
-	}
-	.flexrow{
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-	}
-	.introduction{
-		margin-bottom: 10px;
-	}
-	.address{
 		margin-bottom: 8px;
 	}
 </style>
